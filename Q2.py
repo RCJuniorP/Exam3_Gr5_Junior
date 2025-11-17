44115cb6725f20d394a77e01a0c0aaba19b0289a
RCJuniorP <2541215@cegepoutaouais.qc.ca>
2025-11-17 08:45:18 -0500

    Ajout de gestion d'exception à la fonction (Q2)

--- Q2.py
+++ Q2.py
@@ -2,15 +2,28 @@ import datetime
 import locale
 locale.setlocale(locale.LC_TIME,'')
 
-def afficher_jours_examens(horaire_examen: dict) -> list[str]:
+def afficher_jours_examens(horaire_examen: dict) -> list[str]|None:
     """
     Cette fonction sert à extraire les jours de la semaines où il y a des examens
     :param horaire_examen: dictionnaire contenant les dates d'examens
     :return: une liste de jours de la semaine
     """
     jours = []
+    if not isinstance(horaire_examen, dict): # Cette fonction se retrouve dans la documentation python
+        print("L'horaire d'examen doit être un dictonnaire!")
+        return None
     for examen in horaire_examen:
-        date = datetime.datetime.strptime(horaire_examen[examen], "%d/%m/%Y")
+        if not isinstance(examen, str):
+            print("Les clés dans le dictionnaire de l'horaire d'examen doivent être des string!")
+            return None
+        if not isinstance(horaire_examen[examen], str):
+            print("Les valeurs dans le dictonnaire de l'horaire d'examen doivent être des string!")
+            return None
+        try:
+            date = datetime.datetime.strptime(horaire_examen[examen], "%d/%m/%Y") # Cette fonction crée une exception ValueError lorsque le format est incorrect (voir documentation python)
+        except ValueError:
+            print("Une des dates n'est pas sous le bon format!")
+            return None
         j = date.strftime("%A") # La documentation python sur strptime() a été utilisée pour corriger ces erreurs.
         jours.append(j)
     return jours
@@ -21,4 +34,8 @@ if __name__ == '__main__':
         "anglais" : "12/12/2025",
         "français" : "15/12/2025"
     }
-    print("Les examens sont :", ", ".join(afficher_jours_examens(horaire_examen)))
+    resultat = afficher_jours_examens(horaire_examen)
+    if resultat is None:
+        print("Les données sont invalides!")
+    else:
+        print("Les examens sont :", ", ".join(afficher_jours_examens(horaire_examen)))
--- test_Q2.py
+++ test_Q2.py
@@ -1,10 +1,10 @@
 import pytest
 from Q2 import afficher_jours_examens
 
-@pytest.mark.parametrize("horaire_examen, resultat_attendu", [
+@pytest.mark.parametrize("horaire_examen, resultat_attendu", [ # Méthode vue en classe
     ({"math" : "10/12/2015", "anglais" : "12/12/2025", "français" : "15/12/2025"}, ["jeudi", "vendredi", "lundi"])
 ])
 def test_afficher_jours_examens(horaire_examen, resultat_attendu):
     resultat_obtenu = afficher_jours_examens(horaire_examen)
-    assert type(resultat_obtenu) == type(resultat_attendu)
+    assert isinstance(resultat_obtenu, (list, None))
     assert resultat_obtenu == resultat_attendu
